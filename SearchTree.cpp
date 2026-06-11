3a7632fbf1b429aeaa57dc8c512ebf87c1fe3336
keysha-adesi <keyshaputrii590@gmail.com>
2026-06-11 11:33:41 +0700

    menambahkan Implementasi menu operasi Binary Search Tree menggunakan switch case

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -126,16 +126,92 @@ class BinaryTree
 
         if (ptr != NULL)
         {
+            cout << ptr->info << " ";
+            postorder(ptr->leftcild);
+            postorder(ptr->rightchild);
+            
+        }
+    }
+    void postorder(Node *ptr)
+    {
+        // performs the postorder traversal of the tree
+
+        if (ROOT == NULL)
+        {
+        cout << "Tree is empty" << endl;
+        return;
+        }
+        if (ptr != NULL)
+        {
             postorder(ptr->leftcild);
             postorder(ptr->rightchild);
             cout << ptr->info << " ";
         }
     }
+};
 
+int main ()
+{
+    BinaryTree obj;
+
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout<< "1. implement insert operation" << endl;
+        cout << "2. perform inorder traversal" << endl;
+        cout << "3. perform preorder traversal" << endl;
+        cout << "4. perform postorder traversal" << endl;
+        cout << "5. exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        switch (ch)
+
+        {
+           case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
+        }
+        
+         case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
+
+        case '5':
+            return 0;
+        
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+    return 0;
 }
-
-
-
+        
 
 
 
